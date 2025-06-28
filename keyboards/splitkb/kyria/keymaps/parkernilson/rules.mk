00e9457555afe040d4bad2a159c599a639efb269
Parker Nilson <p_nilson@apple.com>
2025-06-28 07:55:27 -0700

    Add mouse keys to keymap

--- keyboards/splitkb/kyria/keymaps/parkernilson/rules.mk
+++ keyboards/splitkb/kyria/keymaps/parkernilson/rules.mk
@@ -3,3 +3,4 @@ ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 CONVERT_TO = promicro_rp2040
+MOUSEKEY_ENABLE = yes
