5af57e2b2cdc5dd3914f31c51bb14739e65c98cd
Parker Nilson <parker.todd.nilson@gmail.com>
2026-01-01 12:16:20 -0800

    stop using deprecated converter

--- keyboards/splitkb/kyria/keymaps/parkernilson/rules.mk
+++ keyboards/splitkb/kyria/keymaps/parkernilson/rules.mk
@@ -2,5 +2,5 @@ OLED_ENABLE = yes
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
-CONVERT_TO = promicro_rp2040
+CONVERT_TO = sparkfun_pm2040
 MOUSEKEY_ENABLE = yes
